66435338ca438e6dfb3b39448efaaf16cec0c23b
seocodes <azanettev@gmail.com>
2025-07-22 19:11:14 -0300

    delete tweet done - admin can delete any, user can only delete theirsgit add .git add .

--- src/main/java/com/seocodes/spring_security/controller/TweetController.java
+++ src/main/java/com/seocodes/spring_security/controller/TweetController.java
@@ -1,14 +1,15 @@
 package com.seocodes.spring_security.controller;
 
 import com.seocodes.spring_security.controller.dto.CreateTweetDTO;
+import com.seocodes.spring_security.entities.Role;
 import com.seocodes.spring_security.entities.Tweet;
 import com.seocodes.spring_security.repository.TweetRepository;
 import com.seocodes.spring_security.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -36,6 +37,28 @@ public class TweetController {
         tweetRepository.save(tweet);
 
         return ResponseEntity.ok().build();
+    }
 
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            JwtAuthenticationToken token){
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRoles()
+                        .stream()
+                        .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        // Verifica se o tweet pertence àquele usuário - lembrando que o .getName() pega o subject, que nesse caso é o ID
+        // Também verifica se é um Admin (se for, pode excluir também)
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))){
+            tweetRepository.deleteById(tweetId);
+        } else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.ok().build();
     }
 }
